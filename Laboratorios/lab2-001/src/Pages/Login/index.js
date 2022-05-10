e8bee66d4b38e60965a6effca7ab42b8420af30a
Daniel Calvo Marin <dcalvom@gmail.com>
2022-05-10 04:14:56 -0400

    beautify login

--- Laboratorios/lab2-001/src/Pages/Login/index.js
+++ Laboratorios/lab2-001/src/Pages/Login/index.js
@@ -16,43 +16,47 @@ export default function Login() {
   return userIsLoggedIn ? (
     <Navigate to="/" />
   ) : (
-    <div>
-      <div className="mb-4">
-        <input
-          className={`${theme.inputBg} ${theme.inputText}`}
-          value={username}
-          onChange={(evt) => {
-            setUsername(evt.target.value);
+    <div className="flex items-center justify-center h-screen">
+      <div className="text-center">
+        <div className="mb-4">
+          <h1 className="text-3xl font-bold">¡Bienvenido a SwapIt!</h1>
+        </div>
+        <div className="mb-4">
+          <input
+            placeholder="Email"
+            className={`placeholder:text-black pl-4 h-[48px] w-full rounded-md ${theme.inputBg} ${theme.inputText}`}
+            value={username}
+            onChange={(evt) => {
+              setUsername(evt.target.value);
+            }}
+          />
+        </div>
+        <div className="mb-4">
+          <input
+            placeholder="Contraseña"
+            className={`placeholder:text-black pl-4 h-[48px] w-full rounded-md ${theme.inputBg} ${theme.inputText}`}
+            type="password"
+            value={password}
+            onChange={(evt) => {
+              setPassword(evt.target.value);
+            }}
+          />
+        </div>
+        {errorMessage && <span className="text-red-500">{errorMessage}</span>}
+        <button
+          className="h-[48px] w-full rounded-md bg-sky-500 text-white"
+          onClick={() => {
+            dispatch(
+              postLogin({
+                username,
+                password,
+              })
+            );
           }}
-        />
+        >
+          Iniciar Sesión
+        </button>
       </div>
-      <div className="mb-4">
-        <input
-          className={`${theme.inputBg} ${theme.inputText}`}
-          type="password"
-          value={password}
-          onChange={(evt) => {
-            setPassword(evt.target.value);
-          }}
-        />
-      </div>
-      {
-        errorMessage && (
-          <span className="text-red-500" >{errorMessage}</span>
-        )
-      }
-      <button
-        onClick={() => {
-          dispatch(
-            postLogin({
-              username,
-              password,
-            })
-          );
-        }}
-      >
-        Iniciar Sesión
-      </button>
     </div>
   );
 }
